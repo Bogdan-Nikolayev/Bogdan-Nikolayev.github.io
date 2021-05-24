096d1c0c664d0144b095be522f45e18f8d78721c
Bodix <bodix321@gmail.com>
2021-05-24 20:15:57 +0300

    Refactoring

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
+loadScript("https://code.getmdl.io/1.3.0/material.min.js");
+
 function openModel(localPath) {
   window.location.href = "viewer.html?path=" + localPath;
 }
 
 function openPage(href) {
   window.location.href = href;
+}
+
+function loadScript(url) {
+  var head = document.getElementsByTagName("head")[0];
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = url;
+  head.appendChild(script);
 }
