bb10d0416f824ade3f07691ac558bf746a3869eb
Bogdan-Nikolayev <bogdan.nikolayev.work@gmail.com>
2021-06-24 17:28:56 +0300

    Add model-viewer for testing

---
+++ js/test.js
@@ -0,0 +1,7 @@
+/* type User = 1;
+
+function getAdminUser(): User {;
+    return new getAdminUser();
+  } */
+var message = 'Hello, World!';
+console.log(message);
---
+++ js/test.ts
@@ -0,0 +1,8 @@
+/* type User = 1;
+
+function getAdminUser(): User {;
+    return new getAdminUser();
+  } */
+
+let message: string = 'Hello, World!';
+console.log(message);
