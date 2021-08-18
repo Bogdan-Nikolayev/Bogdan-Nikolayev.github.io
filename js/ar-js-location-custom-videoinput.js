27b2ec14046ad4b606e9067f4b52de0f4920c3df
Bogdan-Nikolayev <bogdan.nikolayev.work@gmail.com>
2021-08-18 19:52:18 +0300

    Improve rotation setting

--- js/ar-js-location-custom-videoinput.js
+++ js/ar-js-location-custom-videoinput.js
@@ -8,19 +8,28 @@ this.addEventListener("arjs-video-loaded", () => {
   main.appendChild(videoAr);
 });
 
-document.getElementById("latitude-input").addEventListener("input", function (e) {
-  const gpsCamera = aCamera.getAttribute("gps-camera");
-  gpsCamera.simulateLatitude = e.target.value;
-  aCamera.setAttribute("gps-camera", gpsCamera);
-})
+document
+  .getElementById("latitude-input")
+  .addEventListener("input", function (e) {
+    const gpsCamera = aCamera.getAttribute("gps-camera");
+    gpsCamera.simulateLatitude = e.target.value;
+    aCamera.setAttribute("gps-camera", gpsCamera);
+  });
 
-document.getElementById("longitude-input").addEventListener("input", function (e) {
-  const gpsCamera = aCamera.getAttribute("gps-camera");
-  gpsCamera.simulateLongitude = e.target.value;
-  aCamera.setAttribute("gps-camera", gpsCamera);
-})
+document
+  .getElementById("longitude-input")
+  .addEventListener("input", function (e) {
+    const gpsCamera = aCamera.getAttribute("gps-camera");
+    gpsCamera.simulateLongitude = e.target.value;
+    aCamera.setAttribute("gps-camera", gpsCamera);
+  });
 
-document.getElementById("rotation-input").addEventListener("input", function (e) {
-  aCamera.parentElement.setAttribute('rotation', {x: 0, y: e.target.value, z: 0});
-  console.log(aCamera.object3D.rotation);
-})
+document
+  .getElementById("rotation-input")
+  .addEventListener("input", function (e) {
+    aCamera.parentElement.object3D.rotation.set(
+      0,
+      THREE.Math.degToRad(e.target.value),
+      0
+    );
+  });
