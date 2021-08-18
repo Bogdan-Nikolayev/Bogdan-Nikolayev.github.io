74fb0f675ef206ef5a2cb2f7d532feeed9e99fb8
Bogdan-Nikolayev <bogdan.nikolayev.work@gmail.com>
2021-08-18 19:31:15 +0300

    Add new controls

--- js/ar-js-location-custom-videoinput.js
+++ js/ar-js-location-custom-videoinput.js
@@ -1,3 +1,5 @@
+const aCamera = document.getElementsByTagName("a-camera")[0];
+
 logMediaDevices();
 
 this.addEventListener("arjs-video-loaded", () => {
@@ -5,3 +7,20 @@ this.addEventListener("arjs-video-loaded", () => {
   var main = document.getElementsByClassName("content")[0];
   main.appendChild(videoAr);
 });
+
+document.getElementById("latitude-input").addEventListener("input", function (e) {
+  const gpsCamera = aCamera.getAttribute("gps-camera");
+  gpsCamera.simulateLatitude = e.target.value;
+  aCamera.setAttribute("gps-camera", gpsCamera);
+})
+
+document.getElementById("longitude-input").addEventListener("input", function (e) {
+  const gpsCamera = aCamera.getAttribute("gps-camera");
+  gpsCamera.simulateLongitude = e.target.value;
+  aCamera.setAttribute("gps-camera", gpsCamera);
+})
+
+document.getElementById("rotation-input").addEventListener("input", function (e) {
+  aCamera.parentElement.setAttribute('rotation', {x: 0, y: e.target.value, z: 0});
+  console.log(aCamera.object3D.rotation);
+})
